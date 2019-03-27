bd1d275aa3971b19ba3efd72a032330d5773a7a8
Heo <tjdgus276@naver.com>
2019-03-27 11:39:53 +0900

    added print line

--- test_java_project2/src/test_java_project/test_class.java
+++ test_java_project2/src/test_java_project/test_class.java
@@ -1,5 +1,9 @@
 package test_java_project;
 
-public class test_class {
+public class test_class  {
+	
+	public static void main (String[] args) {
 
+	System.out.println("Hello world~! hotfix!!!");
+	}
 }
