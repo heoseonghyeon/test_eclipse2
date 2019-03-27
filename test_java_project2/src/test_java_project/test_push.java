7be30d650402ef96b4852afe22bdc05aa45bbcc3
heoseonghyeon <48741052+heoseonghyeon@users.noreply.github.com>
2019-03-27 12:15:03 +0900

    Update test_push.java

--- test_java_project2/src/test_java_project/test_push.java
+++ test_java_project2/src/test_java_project/test_push.java
@@ -3,7 +3,9 @@ package test_java_project;
 public class test_push {
 	
 	void func() {
-	 System.out.println("modify at github(server)");
+	        System.out.println("modify at github(server)");
 		System.out.println("im local~!");
+		System.out.println("pull test");
+		
 	}
 }
